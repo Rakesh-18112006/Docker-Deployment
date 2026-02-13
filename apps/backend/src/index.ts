cc7ba0dd97032c77f95d92b392ee54ef8240e696
Rakesh Mummana <mummanarakesh@gmail.com>
2026-02-14 00:31:01 +0530

    v1

---
+++ apps/backend/src/index.ts
@@ -0,0 +1,34 @@
+import express from 'express';
+import "dotenv/config";
+import { prisma } from '@repo/database';
+
+const app = express();
+ 
+app.use(express.json());
+
+app.get('/', ( req , res ) => {
+    res.send('Hello World!');
+});
+
+app.post('signup' , ( req , res ) => {
+    const { username , password } = req.body;
+    if ( !username || !password ) {
+        return res.status(400).json({ message : 'Username and password are required' });
+    }
+   
+     prisma.user.create({
+        data : {
+            username,
+            password 
+        }
+    }).then(user => {
+        res.status(201).json({ message : 'User created successfully', user });
+    }).catch(error => {
+        console.error(error);
+        res.status(500).json({ message : 'Internal server error' });
+    });
+})
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
---
+++ apps/ws-server/src/index.ts
@@ -0,0 +1,27 @@
+import { WebSocketServer } from 'ws';
+import { prisma } from '@repo/database';
+
+const wss = new WebSocketServer({ port: 8080 });
+
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+
+  ws.on('message', async (message) => {
+    console.log(`Received message: ${message}`);
+
+   
+   await prisma.user.create({
+        data : {
+            username: message.toString(),
+            password: Date.now().toString() 
+        }
+   })
+
+    
+    ws.send(`Message received: ${message}`);
+  });
+
+  ws.on('close', () => {
+    console.log('Client disconnected');
+  });
+});
